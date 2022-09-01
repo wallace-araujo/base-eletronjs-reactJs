80184afdb6ed44427bbc927284f810477896980e
wallace araujo <wallace.araujo@ahazou.com>
2022-09-01 17:50:52 -0300

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+
+const routesConfig = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+];
+
+const AppRoute = () => {
+  return (
+    <Routes>
+      {routesConfig.map((options, idx) => (
+        <Route key={`routes-${idx}`} {...options} />
+      ))}
+      <Route path="*" element={<h1>Not found</h1>} />
+    </Routes>
+  );
+};
+
+export default AppRoute;
